4db58dd44be02d70c793f72b80b03dc7692fa5f3
OskarDab <155386183+OskarDab@users.noreply.github.com>
2026-01-19 12:30:20 +0100

    Add application plugin and main class configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
 plugins {
 	java
+	id 'application' 
 	id("org.springframework.boot") version "4.0.0"
 	id("io.spring.dependency-management") version "1.1.7"
 }
-
+repositories{
+	mavenCentral()
+}
 group = "com.project"
 version = "0.0.1-SNAPSHOT"
 description = "Demo project for Spring Boot"
@@ -14,12 +17,16 @@ java {
 	}
 }
 
+
 repositories {
 	mavenCentral()
 }
-
+application{
+	mainClass = 'com.project.TaskManager.Main'
+}
 dependencies {
 	implementation("org.mindrot:jbcrypt:0.2")
+	implementation("org.xerial:sqlite-jdbc:3.42.0.0")
 	implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
