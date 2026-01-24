34813e8f4b7dc1dc23294699dbe19404f35b9461
Piotr K <piotrkapitkowski@gmail.com>
2026-01-24 19:00:48 +0100

    integrate LoginController with database

--- src/main/java/com/project/TaskManager/Controllers/Web/LoginController.java
+++ src/main/java/com/project/TaskManager/Controllers/Web/LoginController.java
@@ -1,4 +1,5 @@
 package com.project.TaskManager.Controllers.Web;
+import com.project.TaskManager.UserManager;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,18 +10,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class LoginController {
+    private final UserManager userMgr;
 
+    public LoginController(UserManager userMgr) {
+        this.userMgr = userMgr;
+    }
     @PostMapping("/login")
     public String valid_login(@RequestParam String login, @RequestParam String password) {
-        if (login.equals("admin") && password.equals("admin")) {
-            return "Zalogowano pomyślnie! Witaj admin.";
+        UserManager userMgr = new UserManager();
+        int userId = userMgr.login(login, password);
+        if (userId != -1) {
+            return "Zalogowano pomyślnie! Witaj " + login +
+                    "\nTwój userId: " + userId;
         } else {
             return "Błąd logowania: Zła nazwa użytkownika lub hasło.";
         }
     }
-    @GetMapping("/register")
-    public String valid_register(@RequestParam String username) {
+    @PostMapping("/register")
+    public String valid_register(@RequestParam String username, @RequestParam String password) {
+        boolean success = userMgr.register(username, password);
 
-            return "Zarejestrowano pomyślnie." + username;
+        if (success) {
+            return "Zarejestrowano pomyślnie: " + username;
+        } else {
+            return "Błąd rejestracji ";
+        }
     }
 }
